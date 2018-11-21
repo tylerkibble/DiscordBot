7f7804c7414ebf87e1ed7da6073c71445a22ca0b
tylerkibble <44215929+tylerkibble@users.noreply.github.com>
2018-11-21 09:54:43 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require(".config.json");
 const prefix = "!";
 client.on("message", (message) => {
   // Exit and stop if it's not there
