c258f0867a094c951d9f5c7b0d3a420e34b14ba7
tylerkibble <44215929+tylerkibble@users.noreply.github.com>
2018-11-21 09:56:31 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,15 +3,15 @@ const client = new Discord.Client();
 const config = require(".config.json");
 const prefix = "!";
 client.on("message", (message) => {
-  // Exit and stop if it's not there
-  if (!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
  
   if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
   } else
-  if (message.content.startsWith(prefix + "IP")) {
-    message.channel.send("IP: SnowsFactions.mcpro.io Enjoy!");
+  if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
   }
 });
  
 client.login("NTE0MjM5OTU0MDg0Mjk4NzUy.DtcnpA._kQAoFTiTfxefQzXuaxsBPAdG8o");
+
